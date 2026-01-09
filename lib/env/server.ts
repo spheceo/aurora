4065b060252ce87d39c39bc3eae5ca89bcaf1f31
S-P-H-E <sphe.g.personal@gmail.com>
2026-01-09 16:09:35 +0200

    Remove commented out clientPrefix and client config

--- lib/env/server.ts
+++ lib/env/server.ts
@@ -3,10 +3,6 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-	// clientPrefix: "NEXT_PUBLIC_",
-	// client: {
-	// 	NEXT_PUBLIC_BASE_URL: z.url(),
-	// },
 	server: {
         SHOPIFY_TOKEN: z.string().min(1),
 		SHOPIFY_DOMAIN: z.url(),
