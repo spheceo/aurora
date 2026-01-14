d3fb1fd3cfd6e5f84169744b7ab3538dd749b2f4
S-P-H-E <sphe.g.personal@gmail.com>
2026-01-14 11:28:02 +0200

    fix fix

--- lib/orpc/index.ts
+++ lib/orpc/index.ts
@@ -4,12 +4,8 @@ import { RPCLink } from "@orpc/client/fetch";
 import { RouterClient } from "@orpc/server";
 import { env } from "../env/web";
 
-const baseUrl = env.NEXT_PUBLIC_BASE_URL
-  ? `https://${env.NEXT_PUBLIC_BASE_URL}`
-  : "http://localhost:3000";
-
 const rpcLink = new RPCLink({
-  url: `${baseUrl}/rpc`,
+  url: `${env.NEXT_PUBLIC_BASE_URL}/rpc`,
 });
 
 export const api: RouterClient<typeof router> = createORPCClient(rpcLink);
